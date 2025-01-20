49e5a8f9d77bd438dc481249d27221fdcad7e3e0
GuiGui2401 <jguiyo@gmail.com>
2025-01-20 18:28:11 +0100

    Fix dependencies

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = require("./app");
 require("dotenv").config();
 require('./utils/cronJob');
 
-const PORT = process.env.PORT || 5002;
+const PORT = process.env.PORT || 3000;
 
 console.log(process.env.NODE_ENV);
 
